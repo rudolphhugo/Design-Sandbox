fb948eb69f471b3a7cbe057f61d78024eae54420
rudolphhugo <53116501+rudolphhugo@users.noreply.github.com>
2026-03-03 11:16:20 +0100

    Revert "Change component grid from 3 to 2 columns"
    This reverts commit 76d98ae9804e77959b8b520f7f633d2ff07a2a5b.

--- app/components/[slug]/page.tsx
+++ app/components/[slug]/page.tsx
@@ -21,7 +21,7 @@ export default async function ComponentPage({ params }: Props) {
       {entry.showcase ? (
         <Component />
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           <Component />
           <Component />
           <Component />
